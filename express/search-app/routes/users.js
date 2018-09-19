cb635dac5e0910aaa1caf2f0f555824ac2c37aff
Krishna Gangaraju <kgangaraju@ebsco.com>
2018-09-20 02:02:25 +0530

    expressjs

---
+++ express/search-app/routes/users.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.get('/:userId/books/:bookId', function (req, res) {
+  res.send(req.params)
+});
+module.exports = router;
